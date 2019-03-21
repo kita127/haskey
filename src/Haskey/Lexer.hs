6d3b282478bdbeb6f5bf1c1593b68c6af1579ca3
kita127 <kitada.kosuke@gmail.com>
2019-03-21 12:32:44 +0900

    lexer 更新

--- src/Haskey/Lexer.hs
+++ src/Haskey/Lexer.hs
@@ -9,14 +9,15 @@ import           Haskey.Token as Tk
 --
 lexer :: T.Text -> [Tk.Token]
 lexer s
-    | T.length s == 0 = []
+    | T.length s == 0 = [eof]
     | otherwise       = let (tk, s') = nextToken s in tk : lexer s'
+  where
+    eof = Tk.Token {Tk.tokenType = Tk.Eof, Tk.literal = ""}
 
 -- | nextToken
 --
 nextToken :: T.Text -> (Tk.Token, T.Text)
 nextToken s
-    | T.length s == 0 = (Tk.Token {Tk.tokenType = Tk.Eof, Tk.literal = ""}, s)
     | ch == '=' = (newToken Tk.Assign ch, T.tail s)
     | ch == ';' = (newToken Tk.Assign ch, T.tail s)
     | ch == '(' = (newToken Tk.Assign ch, T.tail s)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
-import qualified Haskey.Token as Tk
-import qualified Haskey.Lexer as Lx
+{-# LANGUAGE QuasiQuotes       #-}
+import qualified Haskey.Lexer      as Lx
+import qualified Haskey.Token      as Tk
 import           Test.HUnit
+import           Text.RawString.QQ
 
 
 main :: IO ()
@@ -19,50 +21,45 @@ testSample = TestList
   ]
 
 
-testLexerInput = "=+(){},;"
+testLexerInput = [r|let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+    x + y;
+};
+|]
+
 testLexer :: Test
 testLexer = TestList
   [ "testLexer test 1" ~:
         Lx.lexer testLexerInput ~?= [
-          Tk.Token {
-            Tk.tokenType = Tk.Assign
-          , Tk.literal = "="
-          }
+          Tk.Token { Tk.tokenType = Tk.Let , Tk.literal = "let" }
+        , Tk.Token { Tk.tokenType = Tk.Ident , Tk.literal = "five" }
+        , Tk.Token { Tk.tokenType = Tk.Assign , Tk.literal = "=" }
+        , Tk.Token { Tk.tokenType = Tk.Int , Tk.literal = "5" }
 
-        , Tk.Token {
-            Tk.tokenType = Tk.Plus
-          , Tk.literal = "+"
-          }
+        , Tk.Token { Tk.tokenType = Tk.Let , Tk.literal = "let" }
+        , Tk.Token { Tk.tokenType = Tk.Ident , Tk.literal = "ten" }
+        , Tk.Token { Tk.tokenType = Tk.Assign , Tk.literal = "=" }
+        , Tk.Token { Tk.tokenType = Tk.Int , Tk.literal = "10" }
 
-        , Tk.Token {
-            Tk.tokenType = Tk.Lparen
-          , Tk.literal = "("
-          }
-
-        , Tk.Token {
-            Tk.tokenType = Tk.Rparen
-          , Tk.literal = ")"
-          }
-
-        , Tk.Token {
-            Tk.tokenType = Tk.Lbrace
-          , Tk.literal = "{"
-          }
-
-        , Tk.Token {
-            Tk.tokenType = Tk.Rbrace
-          , Tk.literal = "}"
-          }
-
-        , Tk.Token {
-            Tk.tokenType = Tk.Comma
-          , Tk.literal = ","
-          }
-
-        , Tk.Token {
-            Tk.tokenType = Tk.Semicolon
-          , Tk.literal = ";"
-          }
+        , Tk.Token { Tk.tokenType = Tk.Let , Tk.literal = "let" }
+        , Tk.Token { Tk.tokenType = Tk.Ident , Tk.literal = "add" }
+        , Tk.Token { Tk.tokenType = Tk.Assign , Tk.literal = "=" }
+        , Tk.Token { Tk.tokenType = Tk.Function , Tk.literal = "fn" }
+        , Tk.Token { Tk.tokenType = Tk.Lparen , Tk.literal = "(" }
+        , Tk.Token { Tk.tokenType = Tk.Ident , Tk.literal = "x" }
+        , Tk.Token { Tk.tokenType = Tk.Comma , Tk.literal = "," }
+        , Tk.Token { Tk.tokenType = Tk.Ident , Tk.literal = "y" }
+        , Tk.Token { Tk.tokenType = Tk.Rparen , Tk.literal = ")" }
+        , Tk.Token { Tk.tokenType = Tk.Lbrace , Tk.literal = "{" }
+        , Tk.Token { Tk.tokenType = Tk.Ident , Tk.literal = "x" }
+        , Tk.Token { Tk.tokenType = Tk.Plus , Tk.literal = "=" }
+        , Tk.Token { Tk.tokenType = Tk.Ident , Tk.literal = "y" }
+        , Tk.Token { Tk.tokenType = Tk.Semicolon , Tk.literal = ";" }
+        , Tk.Token { Tk.tokenType = Tk.Rbrace , Tk.literal = "}" }
+        , Tk.Token { Tk.tokenType = Tk.Semicolon , Tk.literal = ";" }
+        , Tk.Token { Tk.tokenType = Tk.Eof , Tk.literal = "" }
 
         ]
   ]
