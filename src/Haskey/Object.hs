fd30f23b386544bdcb23a8cbd898446b47632095
kita127 <kitada.kosuke@gmail.com>
2019-05-03 18:18:08 +0900

    null 整数 真偽値のオブジェクトを作成

---
+++ src/Haskey/Object.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Haskey.Object
+(
+) where
+
+import qualified Data.Text as T
+
+data ObjectType = NULL_OBJ
+                | INTEGER
+                | BOOLEAN
+    deriving (Eq, Show)
+
+
+data Object = Null
+            | Integer {
+                intVal :: Integer
+              }
+            | Boolean {
+                boolVal :: Bool
+              }
+            deriving (Eq, Show)
+
+-- | inspect
+--
+inspect :: Object -> T.Text
+inspect Null        = "null"
+inspect (Integer v) = T.pack . show $ v
+inspect (Boolean v) = T.pack . show $ v
+
+
+-- | getObjectType
+--
+getObjectType :: Object -> ObjectType
+getObjectType Null        = NULL_OBJ
+getObjectType (Integer _) = INTEGER
+getObjectType (Boolean _) = BOOLEAN
