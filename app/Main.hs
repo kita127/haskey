917c47410a448268b02ab78a0bf0b6c56c6370c9
kita127 <kitada.kosuke@gmail.com>
2019-06-10 15:13:33 +0900

    コマンドラインオプションを実装

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,43 @@
 module Main where
 
+import           Data.Text.IO        as TIO
 import           Haskey.Repl
+import           Options.Applicative
+
+
+
+-----------------------------------------------------------------------
+-- コマンドラインオプション
+-----------------------------------------------------------------------
+newtype Option = Option
+    { args :: [String]
+    } deriving (Eq, Show)
+
+myOpt :: Parser Option
+myOpt = Option
+    <$> argOption
+--  <*> hogeOption
+    where
+        argOption :: Parser [String]
+        argOption = many $ strArgument
+            $ help "input files"
+            <> metavar "FILE"
+            <> action "file"        -- bash に補完をおまかせする
+
+parserInfo :: ParserInfo Option
+parserInfo = info (helper <*> myOpt)
+    $  fullDesc
+    <> progDesc "Haskey is a programming language written in Haskell"
+    <> header "Haskey programming language"
+
+-----------------------------------------------------------------------
+
+hoge :: String -> IO ()
+hoge path = do
+    contents <- TIO.readFile path
+    TIO.putStrLn contents
+    return ()
+
 
 -- | main
 --
@@ -8,4 +45,8 @@ import           Haskey.Repl
 -- ユーザー名といらっしゃいの挨拶文の出力がまだ未対応
 --
 main :: IO ()
-main = start
+main = do
+    options <- execParser parserInfo
+    if null (args options)
+    then start
+    else hoge $ head $ args options
